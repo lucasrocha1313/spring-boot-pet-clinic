ebfca8b820daa00b11c33d8be4e2e7bb34875788
Tonim <lucasrocha1313@gmail.com>
2021-03-20 10:17:46 -0300

    add specialty to vet

--- pet-clinic-data/src/main/java/br/com/tonim/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/br/com/tonim/petclinic/model/Vet.java
@@ -1,4 +1,13 @@
 package br.com.tonim.petclinic.model;
 
 public class Vet extends Person{
+    private Speciality speciality;
+
+    public Speciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
 }
