022784cc47f8d798ca854aeb8f245e35edfeb161
Tonim <lucasrocha1313@gmail.com>
2021-04-02 13:42:48 -0300

    create visit map service

---
+++ pet-clinic-data/src/main/java/br/com/tonim/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package br.com.tonim.petclinic.services;
+
+import br.com.tonim.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/br/com/tonim/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,36 @@
+package br.com.tonim.petclinic.services.map;
+
+import br.com.tonim.petclinic.model.Visit;
+import br.com.tonim.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null ||
+            visit.getPet().getOwner().getId() == null)
+            throw new RuntimeException("Invalid visit!");
+        return super.save(visit);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+}
