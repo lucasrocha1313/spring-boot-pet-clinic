9937c794b6bb897be994dbde2ce6aa89499f89e6
Tonim <lucasrocha1313@gmail.com>
2021-04-02 10:58:04 -0300

    added spring data jpa service for vets

---
+++ pet-clinic-data/src/main/java/br/com/tonim/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,49 @@
+package br.com.tonim.petclinic.services.springdatajpa;
+
+import br.com.tonim.petclinic.model.Vet;
+import br.com.tonim.petclinic.repositories.VetRepository;
+import br.com.tonim.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        var vets = new HashSet<Vet>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
