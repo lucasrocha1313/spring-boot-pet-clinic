9a34d4eeb2f200f2a121a0983593f19e245b5ec0
Tonim <lucasrocha1313@gmail.com>
2021-05-09 11:36:26 -0300

    Add ability to add or update visit

---
+++ pet-clinic-web/src/main/java/br/com/tonim/petclinic/controllers/VisitController.java
@@ -0,0 +1,52 @@
+package br.com.tonim.petclinic.controllers;
+
+import br.com.tonim.petclinic.model.Visit;
+import br.com.tonim.petclinic.services.PetService;
+import br.com.tonim.petclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+public class VisitController {
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder webDataBinder) {
+        webDataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Map<String, Object> model) {
+        var pet = petService.findById(petId);
+        model.put("pet", pet);
+        var visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String intiNewVisitForm(@PathVariable Long petId, Map<String, Object> model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String proccessNewVisitForm(Visit visit, BindingResult result) {
+        if(result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/";
+        }
+    }
+}
