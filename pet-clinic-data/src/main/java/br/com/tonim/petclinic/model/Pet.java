e62cedd6677bd28ef23d13a9c6d64e7e3c172208
Tonim <lucasrocha1313@gmail.com>
2021-05-19 21:07:37 -0300

    data format on pet

--- pet-clinic-data/src/main/java/br/com/tonim/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/br/com/tonim/petclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package br.com.tonim.petclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -36,6 +37,7 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
